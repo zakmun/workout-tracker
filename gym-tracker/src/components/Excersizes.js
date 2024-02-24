8a8009a95bb433fe37c5fbeb44ea35c9174d9e79
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-24 18:50:21 +0000

    Creating components for home page

---
+++ gym-tracker/src/components/Excersizes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Excersizes = () => {
+  return (
+    <div>Excersizes</div>
+  )
+}
+
+export default Excersizes
---
+++ gym-tracker/src/components/SearchExcersizes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchExcersizes = () => {
+  return (
+    <div>SearchExcersizes</div>
+  )
+}
+
+export default SearchExcersizes
