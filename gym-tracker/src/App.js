f272e589e6e4ccb5ab5c5422a6c97b2375142357
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-24 14:08:30 +0000

    Navbar updates

--- gym-tracker/src/App.js
+++ gym-tracker/src/App.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
-import {Box} from '@mui/material'
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
 
-import './App.css'
+import './App.css';
+import Home from './pages/Home';
+import ExcersizeDetail from './pages/ExcersizeDetail';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
         <Navbar />
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/excersize/:id" element={<ExcersizeDetail />}/>
         </Routes>
+        <Footer/>
     </Box>
   )
 }
