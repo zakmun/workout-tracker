2d8cbafe9732224fd0209eff802edff7f362b929
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-24 18:43:57 +0000

    Navbar styling

--- gym-tracker/src/components/Navbar.js
+++ gym-tracker/src/components/Navbar.js
@@ -7,10 +7,19 @@ import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
   return (
-   <Stack>
+   <Stack direction="row" justifyContent="space-around" sx={{gap: {sm: '122px', xs: '40px'}, mt: {sm: '32px', xs: '20px'}, justifyContent: 'none'}} px="20px">
       <Link to="/">
         <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin: '0 20px'}}/>
       </Link>
+      <Stack
+        direction="row"
+        gap="40px"
+        fontSize="24px"
+        alignItems="flex-end"
+      >
+        <Link to="/" style={{textDecoration: 'none', color: 'black', borderBottom: '3px solid black'}}>Home</Link>
+        <a href="#excersizes" style={{textDecoration: 'none', color: 'black'}}>Excersizes</a>
+      </Stack>
    </Stack>
   )
 }
