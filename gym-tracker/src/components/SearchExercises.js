b46ae9026ad78f75ae03268f7f9745b0de10a4db
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-25 15:41:09 +0000

    importing exerciseOptions

--- gym-tracker/src/components/SearchExercises.js
+++ gym-tracker/src/components/SearchExercises.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
-import { exerciseOptions } from '../utils/fetchData';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const SearchExercises = () => {
   const [search, setSearch] = useState('')
