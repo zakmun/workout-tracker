82548efffc3703590e7fff00d4703e213f33c585
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-25 17:35:41 +0000

    Setting up horizontalscrollbar

--- gym-tracker/src/components/SearchExercises.js
+++ gym-tracker/src/components/SearchExercises.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
 
 const SearchExercises = () => {
@@ -72,6 +73,9 @@ const SearchExercises = () => {
           >
           Search
         </Button>
+      </Box>
+      <Box sx={{positon: 'relative', width: '100%', p: '20px'}}>
+        <HorizontalScrollbar data={bodyParts} />
       </Box>
     </Stack>
   )
