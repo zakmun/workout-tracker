ec45be471c7b5c437b572cd498d0ec310507adac
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-25 15:32:34 +0000

    Getting api from rapidAPI

--- gym-tracker/src/utils/fetchData.js
+++ gym-tracker/src/utils/fetchData.js
@@ -1,4 +1,16 @@
+export const exerciseOptions = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+      'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+  
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
+
+    return data;
 }
