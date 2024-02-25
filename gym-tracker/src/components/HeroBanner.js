c3c23f0d0f47a5685d4530a7fd279b02c317c25e
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-25 14:03:43 +0000

    Hero updates

--- gym-tracker/src/components/HeroBanner.js
+++ gym-tracker/src/components/HeroBanner.js
@@ -5,16 +5,13 @@ import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="orange" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+    <Typography color="orange" fontWeight="600" fontSize="26px">Workout Tracker</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Sweat, Smile <br />
-      And Repeat
-    </Typography>
-    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
-      Check out the most effective exercises personalized to you
+      Track Your <br />
+      Workout
     </Typography>
     <Stack>
-      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: 'orange', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: 'orange', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px'}}>Explore Exercises</a>
     </Stack>
     <Typography fontWeight={600} color="orange" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
       Exercise
