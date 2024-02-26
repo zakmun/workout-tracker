d7233a77ae7cd4df67af1921b94c3f464bd034ca
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-26 20:54:03 +0000

    Adding react scrolling

--- gym-tracker/src/components/HorizontalScrollbar.js
+++ gym-tracker/src/components/HorizontalScrollbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import {ScrollMenu, VisibilityContext} from 'react-horizontal-scrolling-menu';
 
 import BodyPart from './BodyPart';
 
 const HorizontalScrollbar = ({data, BodyParts, setBodyPart}) => {
   return (
-    <div>
+    <ScrollMenu>
         {data.map((item) => (
             <Box
                 key={item.id || item}
@@ -17,7 +18,7 @@ const HorizontalScrollbar = ({data, BodyParts, setBodyPart}) => {
             </Box>
         )
         )}
-    </div>
+    </ScrollMenu>
   )
 }
 
