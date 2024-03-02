4e4a8270b32782f09232b03427d038977feae469
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-02 15:21:54 +0000

    Importing files

--- gym-tracker/src/pages/ExerciseDetail.js
+++ gym-tracker/src/pages/ExerciseDetail.js
@@ -3,10 +3,15 @@ import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+
+    </Box>
+
   )
 }
 
