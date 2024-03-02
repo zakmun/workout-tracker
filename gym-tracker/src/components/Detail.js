66cecc353b7d6e5c776ca689055dc96b8a5c9fa8
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-02 18:42:54 +0000

    Importing

--- gym-tracker/src/components/Detail.js
+++ gym-tracker/src/components/Detail.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Stack, Button } from '@mui/material';
+
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
+
 
 const Detail = () => {
   return (
