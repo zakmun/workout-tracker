e1059ebfc9fa8600e2ba7557d00d6f35f36b1064
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-02 14:42:37 +0000

    Adding styling to exercises and search update

--- gym-tracker/src/components/Exercises.js
+++ gym-tracker/src/components/Exercises.js
@@ -5,9 +5,24 @@ import {Box, Stack, Typography} from '@mui/material';
 import {exersiseOptions, fetchData} from '../utils/fetchData';
 
 
-const Exercises = () => {
+const Exercises = ({exercises, setExercises, bodyPart}) => {
+  console.log(exercises)
   return (
-    <div>Exercises</div>
+    <Box id="exercises"
+    sx={{mt: {lg: '110px'}}}
+    mt="50px"
+    p="20px"
+    >
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack direction="row" sx={{gap: {lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
+        {exercises.map((exercise, index) => (
+          <p>{exercise.name}</p>
+        ))}
+
+      </Stack>
+    </Box>
   )
 }
 
