9765e56b8bd26b67833b7f809e5cec2aa28ddfb7
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-02 14:31:06 +0000

    Importing materials and pages

--- gym-tracker/src/components/Exercises.js
+++ gym-tracker/src/components/Exercises.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import Pagination from '@mui/material/Pagination';
+import {Box, Stack, Typography} from '@mui/material';
+
+import {exersiseOptions, fetchData} from '../utils/fetchData';
+
 
 const Exercises = () => {
   return (
