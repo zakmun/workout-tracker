b085ff31a8389cc5c7b501d38adb5875958e09ee
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-02 14:56:03 +0000

    Adding exercise images

--- gym-tracker/src/components/ExerciseCard.js
+++ gym-tracker/src/components/ExerciseCard.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Button, Stack, Typography} from '@mui/material'; 
 
-const ExerciseCard = () => {
+const ExerciseCard = ({exercise}) => {
   return (
-    <div>ExerciseCard</div>
+    <Link className='exercise-card' to={`/exercise/${exercise.id}`}>
+        <img src={exercise.gifUrl} alt={exercise.name} loading='lazy' />
+    </Link>
   )
 }
 
