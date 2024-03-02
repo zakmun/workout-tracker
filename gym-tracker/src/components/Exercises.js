6f514f87cb897f7dfe0300ce1300127098fa7dea
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-02 15:02:01 +0000

    Removing import

--- gym-tracker/src/components/Exercises.js
+++ gym-tracker/src/components/Exercises.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import Pagination from '@mui/material/Pagination';
 import {Box, Stack, Typography} from '@mui/material';
 
 import {exersiseOptions, fetchData} from '../utils/fetchData';
