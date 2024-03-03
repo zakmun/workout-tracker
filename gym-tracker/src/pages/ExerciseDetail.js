5ce1f8421f1b1cccb57b528096924af82141caa3
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-03 13:23:34 +0000

    Passing the statest

--- gym-tracker/src/pages/ExerciseDetail.js
+++ gym-tracker/src/pages/ExerciseDetail.js
@@ -36,7 +36,7 @@ const ExerciseDetail = () => {
   return (
     <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
       <Detail exerciseDetail={exerciseDetail} />
-      <SimilarExercises />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises}/>
     </Box>
   );
 };
