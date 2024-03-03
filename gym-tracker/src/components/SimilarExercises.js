fcc234cfd6b9ff4e69f4f5ca975bd6a80882c91f
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-03 13:24:23 +0000

    Importing

--- gym-tracker/src/components/SimilarExercises.js
+++ gym-tracker/src/components/SimilarExercises.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Box, Stack } from '@mui/material';
+
+import HorizontalScrollbar from './HorizontalScrollbar';
+import Loader from './Loader';
 
 const SimilarExercises = () => {
   return (
