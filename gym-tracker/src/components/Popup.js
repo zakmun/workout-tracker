2234c8ca1a11242250245f3ba2912c0722d50c01
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-10 15:10:41 +0000

    Adding modal

--- gym-tracker/src/components/Popup.js
+++ gym-tracker/src/components/Popup.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material';
 
-const Popup = () => {
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div>Popup</div>
-  )
+    <React.Fragment>
+      <Button className='search-btn' sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', height: '70px', width: { lg: '200px', xs: '80px' },fontSize: { lg: '20px', xs: '14px' } }} onClick={handleClickOpen}>
+        Track Workout
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const email = formJson.email;
+            console.log(email);
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
 }
-
-export default Popup
