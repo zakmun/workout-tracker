245d1bef0a3016412652fc017e5ddde64a760ce0
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-03-10 15:11:26 +0000

    Removing button from page

--- gym-tracker/src/pages/WorkoutTracker.js
+++ gym-tracker/src/pages/WorkoutTracker.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Button, Stack } from '@mui/material';
 import Popup from '../components/Popup';
 
 const WorkoutTracker = () => {
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', height: '70px', width: { lg: '200px', xs: '80px' },fontSize: { lg: '20px', xs: '14px' } }} >Track My Workout</Button>
+        <Popup/>
     </Stack>
     
   )
